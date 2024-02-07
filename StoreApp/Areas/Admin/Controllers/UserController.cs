4a2f7188818c65fa4c574f501cc37ca806eb8d80
AliBaydur <59739580+AliBaydur@github.com>
2024-02-07 21:34:54 +0300

    Admin Role ataması ve password resetleme işlemleri yazıldı.

--- StoreApp/Areas/Admin/Controllers/UserController.cs
+++ StoreApp/Areas/Admin/Controllers/UserController.cs
@@ -42,5 +42,41 @@ namespace StoreApp.Areas.Admin.Controllers
                 :View();            
         }
 
+        public async Task<IActionResult> Update([FromRoute(Name ="id")] string id)
+        {
+            var user = await _manager.AuthService.GetOneUserForUpdate(id);
+            return View(user);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update([FromForm] UserDtoForUpdate userDto)
+        {
+            if(ModelState.IsValid)
+            {
+                await _manager.AuthService.Update(userDto);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> ResetPassword([FromRoute(Name ="id")] string id)
+        {
+            return View(new ResetPasswordDto()
+            {
+                UserName = id
+            });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ResetPassword([FromForm] ResetPasswordDto model)
+        {
+            var result = await _manager.AuthService.ResetPassword(model);
+            return result.Succeeded 
+                ? RedirectToAction("Index")
+                : View();
+        }
+
     }
 }
